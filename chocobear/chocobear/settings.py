84ecf7372df695a873d934b501b6e4800b3cb8b1
ghosteEddy <eddy.pathit@gmail.com>
2021-11-27 15:34:42 +0700

    secure secret key

--- chocobear/chocobear/settings.py
+++ chocobear/chocobear/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)x$_n)w9_3c$*vqk6-t$!ck(%gu(qny2h7l=ak=sfwpjy&wlfc'
+SECRET_KEY = env('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
